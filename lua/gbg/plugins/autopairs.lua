d1893e9879eb861ce75afab8e79a25bf77aa19da
Guillem Baldi <guillembaldi@gmail.com>
2024-02-24 17:03:21 +0100

    adding autotag and removing lazy-lock

--- lua/gbg/plugins/autopairs.lua
+++ lua/gbg/plugins/autopairs.lua
@@ -8,4 +8,13 @@ return {
             javascript = { "string", "template_string" }
         }
     },
+    config = function()
+        -- If you want insert `(` after select function or method item
+        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+        local cmp = require('cmp')
+        cmp.event:on(
+            'confirm_done',
+            cmp_autopairs.on_confirm_done()
+        )
+    end
 }
---
+++ lua/gbg/plugins/autotag.lua
@@ -0,0 +1,11 @@
+return {
+    "windwp/nvim-ts-autotag",
+    config = function()
+        require('nvim-ts-autotag').setup()
+        require 'nvim-treesitter.configs'.setup {
+            autotag = {
+                enable = true,
+            }
+        }
+    end
+}
